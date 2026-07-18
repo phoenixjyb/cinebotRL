d031b4f1a385692b4cbdd6c06f50ffa00fc00d73
yanbo <yanbophx@hotmail.com>
2026-07-19 04:38:41 +0800

    feat(riser): add bounded case31 canary

--- scripts/two_wheel_balance/run_riser_smoothed_gate_c_camera_lever_arm.sh
+++ scripts/two_wheel_balance/run_riser_smoothed_gate_c_camera_lever_arm.sh
@@ -305,6 +305,17 @@ case "${RISER_CAMERA_LEVER_ARM_GATE_C_AUTHORIZATION:-}" in
     CASE_TIMEOUT_SECONDS=1500
     STAMP="20260719_gate_c_smoothed_case30_v14_camera_lever_arm_v1_exclusive"
     ;;
+  AUTHORIZED_RISER_SMOOTHED_GATE_C_CASE31_V14_CAMERA_LEVER_ARM_V1)
+    PORTFOLIO_STAMP="20260719_smoothed_plan_all79_v14_case22_localized_reversal_cpu"
+    MANIFEST_SHA256="369e3294a45ef468979a81a8bf34b9012f9ec4f77a1d4489c4514930f2d79dab"
+    PLANNER_COMMIT="2e3f769dc8800dd84fc6b6c221b68d0b9a5e7fc0"
+    CASE_A=31
+    CASE_B=""
+    CASE_A_PLAN_SHA256="8ebc938eeb53b8f7dbf4382a085d3667ea38d5ea52e535dc3be409767737aefb"
+    CASE_B_PLAN_SHA256=""
+    CASE_TIMEOUT_SECONDS=1500
+    STAMP="20260719_gate_c_smoothed_case31_v14_camera_lever_arm_v1_exclusive"
+    ;;
   *)
     printf 'camera lever-arm Gate C authorization is absent or unknown\n' >&2
     exit 7
